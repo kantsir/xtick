25e9b660ee9f4d974aba5e6a3b8a620551ea5767
Roman Kantsir <kantsir@roka.local>
2013-10-21 01:11:23 +0300

    added search conditions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,18 @@ class SearchController < ApplicationController
    def search
       search = params[:search]
       tickets=Ticket.arel_table
-      @tickets = Ticket.where(tickets[:body].matches("%#{search}%"))
+
+      subject_matches = match_search_params tickets, :subject, search
+      body_matches = match_search_params tickets, :body, search
+      link_matches = match_search_params tickets, :link, search
+
+      @tickets = Ticket.where(body_matches.or(link_matches.or(subject_matches)))
 
       render 'tickets/index'
    end
+
+  def match_search_params(arel_table, column, search)
+    arel_table[column].matches("%#{search}%")
+  end
 
 end
